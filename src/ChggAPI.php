aeba17eb581066f020293260af3bfbc23ebeb00e
Stefan Wiebe <stefan.wiebe@mindsquare.de>
2017-10-17 16:19:08 +0200

    Added seperate Cache-class for caching and relied on CachingInterface instead of Stash

--- src/ChggAPI.php
+++ src/ChggAPI.php
@@ -6,6 +6,11 @@ use GuzzleHttp\Client;
 
 class ChggAPI {
 	public const
+		CHAMPIONS_ENDPOINT = "champions",
+		MATCHUP_ENDPOINT = "matchup",
+		GENERAL_ENDPOINT = "general",
+		OVERALL_ENDPOINT = "overall",
+
 		CHAMPIONS_LIMIT = "limit",
 		CHAMPIONS_SKIP = "skip",
 		CHAMPIONS_ELO = "elo",
@@ -18,19 +23,16 @@ class ChggAPI {
 	protected $key;
 	protected $client;
 
-	protected $pool;
+	protected $cache;
 
 	function __construct($key) {
 		$this->key = $key;
 		$this->client = new Client(["base_uri" => "api.champion.gg/v2/"]);
 
-		$driver = new \Stash\Driver\FileSystem([
-			"path" => "cache/"
-		]);
-		$this->pool = new \Stash\Pool($driver);
+		$this->cache = new Cache();
 	}
 
-	protected function prepareQuery(array $keys = [], array $params = []) {
+	protected function prepareQuery(array $keys = [], array $params = []) : array {
 		$query = array_fill_keys($keys, null);
 		$query = array_intersect_key($params, $query);
 		$query[self::API_KEY] = $this->key;
@@ -38,22 +40,12 @@ class ChggAPI {
 		return $query;
 	}
 
-	protected static function getCachePath(string $root, array $params = []) {
-		unset($params[self::API_KEY]);
-		return $root . "/" . http_build_query($params);
-	}
-
 	public function getChampions(array $params = []) {
-		$cachePath = self::getCachePath("champions", $params);
-		printf($cachePath);
-
-		$item = $this->pool->getItem($cachePath);
+		$item = $this->cache->getItem(SELF::CHAMPIONS_ENDPOINT, $params);
 
 		if ($item->isHit()) {
-			echo "<br>Cached";
 			$data = $item->get();
 		} else {
-			echo "<br>Not cached";
 			$item->lock();
 
 			$query = $this->prepareQuery(
@@ -70,14 +62,14 @@ class ChggAPI {
 			$data = json_decode($response->getBody());
 
 			$item->set($data);
-			$item->expiresAfter(86400);
-
-			$this->pool->save($item);
-			echo "<br>".date("Y-m-d H:i:s", time())."<br>";
-			echo $item->getExpiration()->format("Y-m-d H:i:s");
+			$this->cache->save($item);
 		}
 
-		printr($data);
+		return $data;
+	}
+
+	public function getCache() : Cache {
+		return $this->cache;
 	}
 }
 
